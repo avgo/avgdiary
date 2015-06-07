56c7d95928f38d66250ac417bdbf596eac937414
Anton Goncharov <gavmail1985@yandex.ru>
2015-06-07 15:35:34 +0300

    Refactoring. Del obsolete or unused subroutines.
    sub action_test;
    sub action_update;
    sub FileProcPrint;
    sub FileProcWriteToFile;
    sub TagsClean;
    sub TagsExpand;
    sub Test;
    sub Update;
    sub UpdateAll;
    sub UpdateSaveToFiles;

--- bin/avg_diary.pl
+++ bin/avg_diary.pl
@@ -2,7 +2,7 @@
 
 #
 # avg-diary - program for text diaries with tags.
-# Anton Goncharov, 2013
+# Anton Goncharov, 2013-2015
 #
 
 BEGIN {
@@ -39,28 +39,16 @@ sub action_add;
 sub action_edit;
 sub action_filename;
 sub action_tags;
-sub action_test;
 sub action_tofb2;
 sub action_tofb2_by_tags;
-sub action_update;
 sub action_view;
 sub action_view_all;
 sub CopyDirStructure($$);
 sub FileCheck;
-sub FileProcPrint;
-sub FileProcWriteToFile;
-sub GenerateFB2;
-sub GenerateFB2Dir;
 sub parse_options;
-sub TagsClean;
-sub TagsExpand;
-sub Test;
 sub ToFB2BT;
 sub ToFB2BTWriteRecords;
 sub ToFB2BTWriteRecordToFile($$$$);
-sub Update;
-sub UpdateAll;
-sub UpdateSaveToFiles;
 
 
 
@@ -165,83 +153,6 @@ sub FileCheck {
 	tags_check \@tags, \@tags_in_file;
 }
 
-sub FileProcPrint {
-	(my $cur_path, my $cur_date, my $cur_record) = @_;
-
-	printf "  %s%ssave to: '%s'%s\n", $cur_record, $mark_green, $cur_path, $mark_e;
-}
-
-sub FileProcWriteToFile {
-	(my $cur_path, my $cur_date, my $cur_record) = @_;
-	my $is_new = (-f $cur_path) ? 0 : 1;
-	
-	open FILE_DAY, ">>", $cur_path or
-			die "не получается открыть файл '$cur_path'. $!.\n";
-	
-	printf FILE_DAY "%s%s",
-		$is_new ? "$cur_date\n\n" : "",
-		$cur_record;
-
-	close FILE_DAY;
-}
-
-sub GenerateFB2 {
-	my $file_fb2;
-	my $file_fb2_filename;
-#	my $data = { };
-
-	$file_fb2_filename = abs_path($tags_dir."/book.fb2");
-	open $file_fb2, ">", $file_fb2_filename or
-			die "ошибка: не получается создать файл '$file_fb2_filename'. $!.\n";
-	printf $file_fb2
-		"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n".
-		"<FictionBook xmlns=\"http://www.gribuser.ru/xml/fictionbook/2.0\" xmlns:l=\"http://www.w3.org/1999/xlink\">\n".
-		"<body>\n";
-	GenerateFB2Dir $file_fb2, $tags_dir, "", "  ";
-	printf $file_fb2
-		"</body>\n".
-		"</FictionBook>\n";
-	close $file_fb2;
-}
-
-sub GenerateFB2Dir {
-	my $file_fb2 = shift;
-	my $cur_dir = shift;
-	my $tag_name = shift;
-	my $indent = shift;
-	my $cur_dir_h;
-	my $cur_file;
-
-	opendir $cur_dir_h, $cur_dir or
-			die "ошибка: не получается открыть каталог '$cur_dir'. $!.\n";
-	while ($cur_file = readdir $cur_dir_h) {
-		next if $cur_file eq "." || $cur_file eq "..";
-
-		my $cur_file2 = abs_path($cur_dir."/".$cur_file);
-		my $sep = $tag_name eq "" ? "" : "/";
-		my $cur_tag = $tag_name.$sep.$cur_file;
-
-		if (-d $cur_file2) {
-			printf $file_fb2
-				"%s<section>\n", $indent;
-			$indent = $indent."  ";
-			printf $file_fb2
-				"%s<title>\n".
-				"%s  <p>%s</p>\n".
-				"%s</title>\n",
-				$indent,
-				$indent,
-				$cur_tag,
-				$indent;
-			printf $file_fb2 "%s<p>section for %s</p>\n", $indent, $cur_tag;
-			GenerateFB2Dir $file_fb2, $cur_file2, $cur_tag, $indent;
-			$indent =~ s/  $//;
-			printf $file_fb2 "%s</section>\n", $indent;
-		}
-	}
-	closedir $cur_dir_h;
-}
-
 sub parse_options {
 	my $opts_hash = shift;
 	my $opts_arr = shift;
@@ -296,48 +207,6 @@ sub TagsAdd {
 	}
 }
 
-sub TagsClean {
-	my $cur_tags_dir = shift;
-	my $tags_dir_h;
-	my $cur_file;
-
-	opendir $tags_dir_h, $cur_tags_dir or
-			die "ошибка: не получается открыть каталог с тегами '$cur_tags_dir'. $!.\n";
-	while ($cur_file = readdir $tags_dir_h) {
-		next if ($cur_file eq "." || $cur_file eq "..");
-		my $cur_file2 = abs_path($cur_tags_dir."/".$cur_file);
-		if (-d $cur_file2) {
-			TagsClean $cur_file2;
-		}
-		elsif ($cur_file =~ /^day/ || $cur_file =~ /^all/) {
-			printf "unlink %s\n", $cur_file2;
-			unlink $cur_file2;
-		}
-	}
-	closedir $tags_dir_h;
-}
-
-sub TagsExpand {
-	my $tags = shift;
-
-	for (my $i = 0; $i <= $#{$tags}; ++$i) {
-		my @new_tags = split /\//, ${$tags}[$i];
-		next if $#new_tags < 1;
-		delete $new_tags[$#new_tags];
-		my $sep = "";
-		my $cur_tag = "";
-	L1:	for (my $j = 0; $j <= $#new_tags; ++$j) {
-			$cur_tag = $cur_tag.$sep.$new_tags[$j];
-			$sep = "/";
-			for my $k (@{$tags}) {
-				next L1 if $cur_tag eq $k;
-			}
-			${$tags}[$#{$tags}+1] = $cur_tag;
-		}
-	}
-	@{$tags} = sort @{$tags};
-}
-
 sub TagsList {
 	my @tags;
 	my @tags_in_file;
@@ -346,19 +215,6 @@ sub TagsList {
 	tags_load_from_file \@tags, $tags_dir;
 	for my $ct (@tags) {
 		printf "$ct\n";
-	}
-}
-
-sub Test {
-	my @tags = (
-		"tag1/tag11",
-		"tag1/tag11/tag111",
-		"tag1/tag11/tag111/tag1111/tag11111/tag111111/tag1111111/tag11111111"
-	);
-	TagsExpand \@tags;
-
-	for my $tag (@tags) {
-		printf "%s\n", $tag;
 	}
 }
 
@@ -504,172 +360,6 @@ sub ToFB2BTWriteRecordToFile($$$$) {
 	close $fd;
 }
 
-sub Update {
-	my @tags;
-	my $diary_dir_h;
-	my $cur_file;
-	my @files;
-
-	CheckCreateTagsPath;
-	TagsClean $tags_dir;
-	tags_load_from_file \@tags, $tags_dir;
-	opendir $diary_dir_h, $avg_diary_dir or die "Не получается открыть каталог '$avg_diary_dir'. $!.\n";
-	while ($cur_file = readdir $diary_dir_h) {
-		if ($cur_file =~ /^day_/) {
-			push @files, $cur_file;
-		}
-	}
-	closedir $diary_dir_h;
-
-	my $file_proc = \&FileProcWriteToFile;
-
-	my $state_init = 1;
-	my $state_date = 2;
-	my $state_rec = 3;
-
-	my $state = $state_init;
-
-	@files = sort @files;
-	for my $file1 (@files) {
-		my $file2 = abs_path(${avg_diary_dir}."/".${file1});
-		my $cur_date;
-		my $cur_record;
-		my @tags_in_record;
-
-		open DAY1, "<", $file2 or die "Не получается открыть файл '$file2'. $!.\n";
-		my $line_num = 1;
-		while (my $line = <DAY1>) {
-			given ($state) {
-			when ($state_init) {
-				given ($line) {
-				when (/^[0-9]{2}\.[0-9]{2}\.[0-9]+/) {
-					chomp $line;
-					$cur_date = $line;
-					$state = $state_date;
-				}
-				when (/^$/) {
-				}
-				default {
-					printf "%s:%u: нарушение синтаксиса.\n", $file2, $line_num;
-					exit 1;
-				}
-				}
-			}
-			when ($state_date) {
-				given ($line) {
-				when (/^[0-9]{2}\.[0-9]{2}\.[0-9]+/) {
-					chomp $line;
-					$cur_date = $line;
-				}
-				when (/^[0-9]{2}:[0-9]{2}/) {
-					$cur_record = $line;
-					$state = $state_rec;
-				}
-				when (/^$/) {
-				}
-				default {
-					printf "%s:%u: нарушение синтаксиса.\n", $file2, $line_num;
-					exit 1;
-				}
-				}
-			}
-			when ($state_rec) {
-				given ($line) {
-				when (/^[0-9]{2}\.[0-9]{2}\.[0-9]+/) {
-					UpdateSaveToFiles \@tags, \@tags_in_record, $cur_date, $cur_record, $file_proc;
-					@tags_in_record = ();
-					chomp $line;
-					$cur_date = $line;
-					$state = $state_date;
-				}
-				when (/^[0-9]{2}:[0-9]{2}/) {
-					UpdateSaveToFiles \@tags, \@tags_in_record, $cur_date, $cur_record, $file_proc;
-					@tags_in_record = ();
-					$cur_record = $line;
-				}
-				when (/^ +tags:/) {
-					parse_line_with_tags \@tags_in_record, $line, "$file2:$line_num";
-					$cur_record = $cur_record.$line;
-				}
-				default {
-					$cur_record = $cur_record.$line;
-				}
-				}
-			}
-			}
-			++$line_num;
-		}
-		close DAY1;
-		if ($state == $state_rec) {
-			UpdateSaveToFiles \@tags, \@tags_in_record, $cur_date, $cur_record, $file_proc;
-			$state = $state_init;
-		}
-	}
-	UpdateAll $tags_dir;
-	GenerateFB2;
-}
-
-sub UpdateAll {
-	my $cur_tags_dir = shift;
-	my $tags_dir_h;
-	my $cur_file;
-
-	opendir $tags_dir_h, $cur_tags_dir or
-			die "ошибка: не получается открыть каталог с тегами '$cur_tags_dir'. $!.\n";
-	my $cur_all_filename = abs_path($cur_tags_dir."/all.txt");
-	open my $file_all, ">>", $cur_all_filename or
-			die "ошибка: не получается открыть файл '$cur_all_filename'. $!.\n";
-	while ($cur_file = readdir $tags_dir_h) {
-		next if ($cur_file eq "." || $cur_file eq "..");
-		my $cur_file2 = abs_path($cur_tags_dir."/".$cur_file);
-		if (-d $cur_file2) {
-			UpdateAll $cur_file2;
-		}
-		elsif ($cur_file =~ /^day_/) {
-			open FILE_DAY, "<", $cur_file2 or
-					die "ошибка: не получается открыть файл '$cur_file2'. $!.\n";
-			my $line;
-			while ($line = <FILE_DAY>) {
-				printf $file_all "%s", $line;
-			}
-			close FILE_DAY;
-		}
-	}
-	closedir $tags_dir_h;
-	close $file_all;
-}
-
-sub UpdateSaveToFiles {
-	(my $tags, my $tags_in_record, my $cur_date, my $cur_record, my $file_proc) = @_;
-
-	if ($cur_date eq "") {
-		printf "date must be not empty.\n";
-		exit(1);
-	}
-
-	if (!tags_check $tags, $tags_in_record) {
-		chomp $cur_date;
-		printf "exit. date: %s\n", $cur_date;
-		exit(1);
-	}
-
-	TagsExpand $tags_in_record;
-
-	$cur_date =~ /([0-9]+)\.([0-9]+)\.([0-9]+)/;
-	my $filename1 = "day_$3_$2_$1.txt";
-
-	if (scalar @{$tags_in_record} == 0) {
-		my $cur_path = abs_path($avg_diary_dir."/tags/".$filename1);
-		&{$file_proc}($cur_path, $cur_date, $cur_record);
-	}
-	else {
-		for my $cur_tag (@{$tags_in_record}) {
-			my $cur_path = abs_path($avg_diary_dir."/tags/".$cur_tag."/".$filename1);
-			&{$file_proc}($cur_path, $cur_date, $cur_record);
-		}
-	}
-}
-
 
 
 
@@ -714,10 +404,8 @@ my %actions = (
 	"edit"		=> \&action_edit,
 	"filename"	=> \&action_filename,
 	"tags"		=> \&action_tags,
-	"test"		=> \&action_test,
 	"tofb2"		=> \&action_tofb2,
 	"tofb2-by-tags"	=> \&action_tofb2_by_tags,
-	"update"	=> \&action_update,
 	"view"		=> \&action_view,
 	"view-all"	=> \&action_view_all,
 	"viewall"	=> \&action_view_all
@@ -765,10 +453,6 @@ sub action_tags {
 	}}
 }
 
-sub action_test {
-	Test;
-}
-
 sub action_tofb2 {
 	if (scalar @ARGV != 1) {
 		printf "ошибка: нужно указать путь к fb2-книге.\n";
@@ -787,10 +471,6 @@ sub action_tofb2_by_tags {
 	}
 	my $fb2_arg = shift @ARGV;
 	ToFB2BT $fb2_arg;
-}
-
-sub action_update {
-	Update;
 }
 
 sub action_view {
