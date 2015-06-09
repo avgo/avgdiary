73c10de76248cb04d61badc830c8874397b1ec1f
Anton Goncharov <gavmail1985@yandex.ru>
2015-06-09 10:37:46 +0300

    Refactoring. Move fb2_by_tags to separate module.

--- bin/avg_diary.pl
+++ bin/avg_diary.pl
@@ -18,6 +18,7 @@ use File::Spec;
 use POSIX qw(strftime);
 use List::MoreUtils qw(uniq);
 
+use avg_diary::fb2::bytags;
 use avg_diary::fb2::normal;
 use avg_diary::reader;
 use avg_diary::tags;
@@ -46,9 +47,6 @@ sub action_view_all;
 sub CopyDirStructure($$);
 sub FileCheck;
 sub parse_options;
-sub ToFB2BT;
-sub ToFB2BTWriteRecords;
-sub ToFB2BTWriteRecordToFile($$$$);
 
 
 
@@ -218,148 +216,6 @@ sub TagsList {
 	}
 }
 
-sub ToFB2BT {
-	my $fb2_filename = shift;
-
-	my $tmp_dir = "/tmp/diary-fb2.tmp";
-	system "rm -rf $tmp_dir";
-	mkdir $tmp_dir or die sprintf(
-			"ошибка: не получается создать каталог '%s'. %s.\n",
-			$tmp_dir, $!);
-	CheckCreateTagsPath $tags_dir;
-	CopyDirStructure $tags_dir, $tmp_dir;
-	
-	my $reader = avg_diary::reader->new(
-			avg_diary_dir => $avg_diary_dir,
-			cut_time => 1,
-			cut_spaces => 1);
-	$reader->first;
-	while (my $rec = $reader->fetch) {
-		my $tags = $rec->{tags};
-		my $date = $rec->{date};
-		$date =~ /0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)/;
-		my $day_filename = sprintf "day_%04u_%02u_%02u.html", $3, $2, $1;
-		if ($#{$tags} >= 0) {
-			for my $cur_tag (@{$tags}) {
-				my $filename = abs_path(
-						$tmp_dir."/".$cur_tag."/".$day_filename);
-				ToFB2BTWriteRecordToFile
-						$filename,
-						$rec->{date},
-						$rec->{time},
-						$rec->{record};
-			}
-		}
-		else {
-			my $filename = abs_path($tmp_dir."/".$day_filename);
-			ToFB2BTWriteRecordToFile
-					$filename,
-					$rec->{date},
-					$rec->{time},
-					$rec->{record};
-		}
-	}
-
-	open my $fb2_file, ">", $fb2_filename or die sprintf(
-			"ошибка: не получается открыть файл '%s'. %s.\n",
-			$fb2_filename, $!);
-	printf $fb2_file
-		"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n".
-		"<FictionBook xmlns=\"http://www.gribuser.ru/xml/fictionbook/2.0\" xmlns:l=\"http://www.w3.org/1999/xlink\">\n".
-		"<body>\n";
-	ToFB2BTWriteRecords $fb2_file, $tmp_dir, "";
-	printf $fb2_file
-		"</body>\n".
-		"</FictionBook>\n";
-	close $fb2_file;
-	system "rm -rf $tmp_dir";
-}
-
-sub ToFB2BTWriteRecords {
-	(my $fb2_file, my $dir, my $tag) = @_;
-
-	my @dirs;
-	my @files;
-
-	opendir my $dir_h, $dir or die sprintf(
-			"ошибка: не получается открыть каталог '%s'. %s.\n",
-			$dir, $!);
-	while (my $cur_file = readdir $dir_h) {
-		next if $cur_file eq "." or $cur_file eq "..";
-		my $cur_file2 = abs_path($dir."/".$cur_file);
-		if (-d $cur_file2) {
-			push @dirs, $cur_file;
-		}
-		elsif (-f $cur_file2 && $cur_file =~ /^day_/) {
-			push @files, $cur_file;
-		}
-	}
-	closedir $dir_h;
-
-	for my $cur_dir (sort @dirs) {
-		my $cur_dir2 = abs_path($dir."/".$cur_dir);
-		my $cur_tag = ($tag eq "") ? $cur_dir : $tag."/".$cur_dir;
-		ToFB2BTWriteRecords $fb2_file, $cur_dir2, $cur_tag;
-	}
-
-	my $tag_text;
-
-	if ($tag eq "") {
-		$tag_text = "без тега";
-	}
-	else {
-		$tag_text = $tag;
-		$tag_text =~ s/&/&amp;/g;
-		$tag_text =~ s/</&lt;/g;
-		$tag_text =~ s/>/&gt;/g;
-	}
-
-	printf $fb2_file
-		"<section>\n".
-		"<title>\n".
-		"  <p>%s</p>\n".
-		"</title>\n", $tag_text;
-	for my $cur_file (sort @files) {
-		my $cur_file2 = abs_path($dir."/".$cur_file);
-		open my $fd, "<", $cur_file2 or
-				die sprintf ("ошибка: не получается открыть файл '%s'. %s.\n",
-				$cur_file2, $!);
-		while (my $line = <$fd>) { print $fb2_file "  ",$line; }
-		close $fd;
-	}
-	printf $fb2_file "</section>\n";
-}
-
-sub ToFB2BTWriteRecordToFile($$$$) {
-	(my $filename, my $date, my $time, my $record) = @_;
-	my $new_file = (-f $filename) ? 0 : 1;
-
-	$record =~ s/&/&amp;/g;
-	$record =~ s/</&lt;/g;
-	$record =~ s/>/&gt;/g;
-	$record =~ s/$/<\/v>/mg;
-	$record =~ s/^/  <v>/mg;
-
-	open my $fd, ">>", $filename or die
-		sprintf("ошибка: не получается открыть файл '%s'. %s.\n", $filename, $!);
-	printf $fd
-		"<p>\n".
-		"  <strong>%s</strong>\n".
-		"</p>\n",
-		$date if $new_file;
-	printf $fd
-		"<p>\n".
-		"  <strong>%s</strong>\n".
-		"</p>\n".
-		"<poem>\n".
-		"<stanza>\n".
-		"%s\n".
-		"</stanza>\n".
-		"</poem>\n",
-		$time, $record;
-	close $fd;
-}
-
 
 
 
@@ -470,7 +326,7 @@ sub action_tofb2_by_tags {
 		exit(1);
 	}
 	my $fb2_arg = shift @ARGV;
-	ToFB2BT $fb2_arg;
+	fb2_by_tags $fb2_arg;
 }
 
 sub action_view {
