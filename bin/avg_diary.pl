6d6368df2b0935354573cab133226bd604e13f23
Anton Goncharov <gavmail1985@yandex.ru>
2016-03-17 23:31:21 +0300

    Record addition: initial.
    New nethods is action_add() and avg_diary_add().
    New way for records addition!

--- bin/avg_diary.pl
+++ bin/avg_diary.pl
@@ -13,6 +13,20 @@ BEGIN {
 use strict;
 
 use Cwd qw(abs_path);
+use File::Basename;
+use File::Spec;
+use POSIX qw(strftime);
+use List::MoreUtils qw(uniq);
+
+use avg_diary::tags::utils;
+
+
+
+
+sub action_add;
+sub avg_diary_add;
+sub avg_diary_dir_env;
+sub print_usage;
 
 
 
@@ -23,6 +37,183 @@ my $mark_e = "\033[0m";
 
 
 
+
+sub action_add {
+	my $param_uptime;
+	my $param_dd;
+	my $param_mon;
+	my $param_yyyy;
+	my $param_hh;
+	my $param_min;
+
+	my %cnf;
+
+	while ($_ = shift @_)
+	{
+		if ($_ eq "--uptime")
+		{
+			die "error: uptime is already set!\n"
+					if $param_uptime == 1;
+			die "error: only uptime or date can be set!\n"
+					if defined $param_dd;
+			die "error: only uptime or time can be set!\n"
+					if defined $param_hh;
+			$param_uptime = 1;
+		}
+		elsif (/^([0-9]{1,2})\.([0-9]{1,2})(\.([0-9]{1,4}))?$/)
+		{
+			die "error: only uptime or date can be set!\n"
+					if $param_uptime == 1;
+			die "error: date is already defined!\n"
+					if defined $param_dd;
+			($param_dd, $param_mon, $param_yyyy) = ($1, $2, $4);
+		}
+		elsif (/^([0-9]{1,2}):([0-9]{1,2})$/)
+		{
+			die "error: only uptime or date can be set!\n"
+					if $param_uptime == 1;
+			die "error: time is already set!\n"
+					if defined $param_hh;
+			($param_hh, $param_min) = ($1, $2);
+		}
+		else
+		{
+			die "error: unknown parameter '$_'.\n";
+		}
+	}
+
+	#
+	# today is 17.03.2016 10:49.
+	#
+	#   avg-diary add --uptime
+	#   avg-diary add 01.02.2011       -- please, specify a datetime.
+	#   avg-diary add 01.02.2011 10:00 -- ok.
+	#
+	# alternative:
+	#
+	#   avg-diary add 01.02.2011       -- empty record?
+	#
+
+	if (defined $param_dd)
+	{
+		die "error: empty times is not supported.\n"
+				if not defined $param_hh;
+
+		if (0 <= $param_yyyy && $param_yyyy <= 50)
+		{
+			$param_yyyy += 2000;
+		}
+		elsif (51 <= $param_yyyy && $param_yyyy <= 99)
+		{
+			$param_yyyy += 1900;
+		}
+	}
+	elsif (not defined $param_uptime)
+	{
+		(	my $now_sec,  my $now_min,  my $now_hour,
+			my $now_mday, my $now_mon,  my $now_year,
+			my $now_wday, my $now_yday, my $now_isdst) = localtime;
+
+		$now_mon  += 1;
+		$now_year += 1900;
+
+		$param_dd   = $now_mday;
+		$param_mon  = $now_mon;
+		$param_yyyy = $now_year;
+
+		if (not defined $param_hh)
+		{
+			$param_hh    = $now_hour;
+			$param_min   = $now_min;
+		}
+
+	}
+
+	if (defined $param_uptime)
+	{
+		$cnf{uptime} = 1;
+	}
+	else
+	{
+		my $leap_y;
+		my $at_ly;
+
+		my $day_min = 1;
+		my $day_max;
+
+		$leap_y = ($param_yyyy % 4 == 0) ? 1 : 0;
+
+		if ($param_mon < 1 or 12 < $param_mon)
+		{
+			die "error: incorrect value of month!\n";
+		}
+
+		if (	($param_mon == 4) or ($param_mon == 6) or ($param_mon == 9) or
+			($param_mon == 11) )
+		{
+			$day_max = 30;
+		}
+		elsif ($param_mon == 2)
+		{
+			if ($leap_y)
+			{
+				$at_ly   = " at leap year";
+				$day_max = 29;
+			}
+			else
+			{
+				$at_ly   = " at not leap year";
+				$day_max = 28;
+			}
+		}
+		else
+		{
+			$day_max = 31;
+		}
+
+		die "error: month $param_mon$at_ly must have a day between $day_min and $day_max.\n"
+			if $param_dd < $day_min or $day_max < $param_dd;
+
+		die "error: wrong hours.\n"
+			if $param_hh < 0 or 23 < $param_hh;
+
+		die "error: wrong minutes.\n"
+			if $param_min < 0 or 59 < $param_min;
+
+		$cnf{date} = [
+			$param_dd,
+			$param_mon,
+			$param_yyyy,
+			$param_hh,
+			$param_min,
+		];
+	}
+
+	avg_diary_add %cnf;
+}
+
+sub avg_diary_add {
+	(my %cnf) = @_;
+
+	my $uptime = delete $cnf{uptime};
+	my $date   = delete $cnf{date};
+
+	if (defined $uptime)
+	{
+		die "error: uptime and date def.\n"
+				if defined $date;
+
+		print "uptime.\n";
+	}
+	else {
+		die "error: one of uptime or date must be def.\n"
+				if not defined $date;
+
+		printf	"%02u.%02u.%02u %02u:%02u\n",
+			${$date}[0], ${$date}[1], ${$date}[2],
+			${$date}[3], ${$date}[4];
+	}
+}
 
 sub avg_diary_dir_env {
 	my $avg_diary_dir = $ENV{avg_diary_dir};
@@ -57,10 +248,9 @@ if (scalar @ARGV == 0) {
 
 my $command = shift @ARGV;
 
-my %actions = ( );
-
-
-
+my %actions = (
+	"add" => \&action_add,
+);
 
 my $proc = $actions{$command};
 
@@ -77,5 +267,5 @@ if (not defined $proc) {
 	exit(1);
 }
 
-&{$proc};
+&{$proc} (@ARGV);
 
